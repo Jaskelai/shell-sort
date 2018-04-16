70f76cc8576c94ae8c079cd80b08723107a63401
Azat Mukhametzaynov <azatmukhametzyanov@ya.ru>
2018-04-16 20:11:25 +0300

    sort of list removed

--- src/main/java/me/paradoxalen/shellsort/Sort.java
+++ src/main/java/me/paradoxalen/shellsort/Sort.java
@@ -4,6 +4,5 @@ import java.util.Comparator;
 import java.util.List;
 
 public interface Sort {
-    <T> void sort(List<T> list, Comparator<? super T> comparator);
     <T> void sort(T[] array, Comparator<? super T> comparator);
 }
