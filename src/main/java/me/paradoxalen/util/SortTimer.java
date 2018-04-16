db4df27883325afb7defd0d6f796db0ad5bf217c
Azat Mukhametzaynov <azatmukhametzyanov@ya.ru>
2018-04-16 20:36:20 +0300

    timer for list removed

--- src/main/java/me/paradoxalen/util/SortTimer.java
+++ src/main/java/me/paradoxalen/util/SortTimer.java
@@ -7,24 +7,10 @@ import java.util.Comparator;
 import java.util.List;
 
 public class SortTimer {
-    public static <T> long getSortingTimeMillis(Sort sort, List<T> source, Comparator<T> comparator) {
-        long start = System.currentTimeMillis();
-        sort.sort(source, comparator);
-        long end = System.currentTimeMillis();
-        return end - start;
-    }
-
     public static <T> long getSortingTimeMillis(Sort sort, T[] source, Comparator<T> comparator) {
         long start = System.currentTimeMillis();
         sort.sort(source, comparator);
         long end = System.currentTimeMillis();
-        return end - start;
-    }
-
-    public static <T> long getSortingTimeNano(Sort sort, List<T> source, Comparator<T> comparator) {
-        long start = System.nanoTime();
-        sort.sort(source, comparator);
-        long end = System.nanoTime();
         return end - start;
     }
 
